f73842625d2be38831397d61585ebc2154317ba9
Albert Saucedo <saucedoa7@gmail.com>
2014-09-13 20:10:21 -0500

    Initial Commit

---
+++ Texter/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Texter
+//
+//  Created by Albert Saucedo on 9/13/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
